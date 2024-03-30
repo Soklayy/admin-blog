b45a64f14a44c7217a5424392d82f9c87d006a20
Soklayy <soklayiter34@gmail.com>
2024-03-31 00:25:55 +0700

    modified axios instance

--- src/api/axios.instance.ts
+++ src/api/axios.instance.ts
@@ -3,6 +3,7 @@ import axios from "axios";
 export const axiosInstance = axios.create({
     baseURL: process.env.API,
     headers: {
+        Authorization:localStorage.getItem('token'),
         'Access-Control-Allow-Origin': process.env.API
     },
 })
